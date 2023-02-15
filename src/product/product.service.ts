d7ae3cdbcf162a652b9ce60e1de356a4ed745034
Hieu <tdhieu23@gmail.com>
2023-02-15 08:56:46 +0700

    Update

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import Product from './entities/product.entity';
+import { Repository } from 'typeorm';
+import Category from 'src/category/category.entities';
+import { HttpException } from '@nestjs/common/exceptions';
+import { HttpStatus } from '@nestjs/common/enums';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    private readonly productRepository: Repository<Product>,
+    private readonly categoryRepository: Repository<Category>
+  ) {}
+
+  async create(product: CreateProductDto) {
+    const category = await this.categoryRepository.findOne({where: {id: product.categoryId}})
+    if(!category) throw new HttpException('Not found category', HttpStatus.NOT_FOUND)
+   
+    const newProduct = await this.productRepository.create({...product})
   }
 
   findAll() {
